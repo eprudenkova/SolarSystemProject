12fded6a29e0611c1c50fc8c70f0fc4feda4e933
Evgeniya Prudenkova <evgeniya@MacBook-Pro-16.lan1>
2021-01-26 20:40:18 -0500

    first commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem.sun = 1;
+        solarSystem.moon = 1;
+        solarSystem.planets = 3;
+        
+    }
+}
